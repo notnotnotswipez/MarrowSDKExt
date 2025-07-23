0c18dab6e29f29e05f18679587a1e1d8917a8e65
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-07-23 10:24:49 -0400

    Add missing fields to CircuitSocket and ButtonController.

--- Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/CircuitSocket.cs
+++ Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/CircuitSocket.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
+using SLZ.Marrow;
 
 namespace SLZ.Marrow.Circuits
 {
     public class CircuitSocket : Circuit
     {
+        [SerializeField]
+        private Circuit _input;
+
+        [SerializeField]
+        protected Servo _servo;
+
+        [SerializeField]
+        protected Rigidbody _rb;
+
+        public Circuit input
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+            }
+        }
     }
 }
